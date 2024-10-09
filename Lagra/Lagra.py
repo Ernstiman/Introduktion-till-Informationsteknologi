30d78c527bec5da8325ec1fc50bf5196da1d4cf1
Viktor <viktor@ekastigen.net>
2024-10-09 12:38:41 +0200

    lagra är nu fan skit bra!!

--- Lagra/Lagra.py
+++ Lagra/Lagra.py
@@ -3,62 +3,69 @@ import os
 
 class User():
 
-  def __init__(self):
-    self.username = "Viktor"
-    self.password = "1234"
+  def __init__(self, username, password):
+    self.username = username
+    self.password = password
     self.items = []
 
   def importItems(self, file):
     items = file.readline()
     items = items.split()
     for item in items:
-      if item not in items:
+      if item not in self.items:
         self.items.append(item)
 
 
 def checkForFile(user):
   items_file_name = f'{user.username}_items.txt'
   login_file_name = f'{user.username}_login.txt'
-  list = os.listdir(os.getcwd())
+  login_list = os.listdir(os.getcwd() + "/logins")
+  item_list = os.listdir(os.getcwd() + "/items")
 
-  if(items_file_name not in list):
-    items_file = open(items_file_name, "x+")
+  if(items_file_name not in item_list):
+    items_file = open(os.getcwd() + "/items/" + items_file_name, "x+")
   else: 
-    items_file = open(items_file_name, "r+")
+    items_file = open(os.getcwd() + "/items/" + items_file_name, "r+")
 
-  if(login_file_name not in list):
-    login_file = open(login_file_name, "x+")
+  if(login_file_name not in login_list):
+    login_file = open(os.getcwd() + "/logins/" + login_file_name, "x+")
   else: 
-    login_file = open(login_file_name, "r+")
+    login_file = open(os.getcwd() + "/logins/" + login_file_name, "r+")
 
   return(login_file, items_file)
 
+def checkAccountDetails(users, username, password):
+  for user in users:
+    if (user.username == username and user.password == password):
+      return True
+  return False
 
-  
-def logIn(user):
+def logIn(users):
   print("")
   username = input("User: ")
   password = input("Password:")
 
-  if(user.username == username and user.password == password):
-    print("")
-    print(f'Välkommen {username}')
-    print("")
-    menu(user)
+  if checkAccountDetails(users, username, password):
+    for user in users:
+      if (user.username == username and user.password == password):
+        print("")
+        print(f'Välkommen {username}')
+        print("")
+        menu(user)
   else:
     print("\nInvalid username or password!\n")
-    tryAgain(logIn, user)
+    tryAgain(logIn, users)
 
 def checkIput():
   print("\nDu behöver ange ett av de angivna valen!!!\n")
   
 
-def tryAgain(function, user = None):
+def tryAgain(function, users = None):
   print("r) Try again")
   print("q) Quit ")
   val = input("\nOption: ")
   if val == "r":
-    function(user)
+    function(users)
   elif val != "q":
     checkIput()
 
@@ -82,12 +89,15 @@ def menu(user):
       checkIput()
     pass
 
-def quit(login_file, items_file,user):
+def saveLogin(file, user):
   login_text = f'{user.username} {user.password}'
-  item_text = " ".join(user.items)
-  login_file.write(login_text)
+  file.write(login_text)
+  file.close()
+  pass
+
+def quit(login_file, items_file,user):
+  item_text = " " + " ".join(user.items)
   items_file.write(item_text)
-  login_file.close()
   items_file.close()
 
 def addItem(user):
@@ -95,21 +105,12 @@ def addItem(user):
   user.items.append(item)
   print(f'\n{item} has been added\n')
 
-
-
 def fileOpener(user):
   name = f'{user.username}_items.txt'
   file = open(name, "r+")
   return file
   pass
-def write(file):
-  pass
 
-def read(file):
-  pass
-
-def createFile(file):
-  pass
 
 def listItems(user):
   index = 1
@@ -117,20 +118,50 @@ def listItems(user):
   for item in user.items:
     print(f'{index}. {item}')
     index += 1
-  pass
+
+def lookThroughFiles(users):
+  for files in os.listdir(os.getcwd() + "/logins"):
+      my_files = open(os.getcwd() + "/logins/" + files, "r")
+      files = my_files.readline()
+      files = files.split()
+      print(files)
+      users.append(User(files[0], files[1]))
+      my_files.close()
+
+def createAccount(users):
+  username = input("Enter your username: ")
+  password = input("Enter your password: ")
+
+  while True:
+    val = input(f'Are you sure you want the username: {username} and the password: {password}(y/n)? ')
+    if val == "y":
+      new_user = User(username, password)
+      users.append(new_user)
+      loginFile,_ = checkForFile(new_user)
+      saveLogin(loginFile, new_user)
+      break
+    elif val == "n":
+      createAccount(users)
+      break
+    else: 
+      checkIput()
 
 
 def main():
-  viktor = User()
+  users = []
+  lookThroughFiles(users)
   while True: 
     print("\nWelcome to Lagra (TM) \n")
     print("l) Log in")
+    print("c) Create account")
     print("q) Quit")
     val = input("\nOption: ")
     if val == "l": 
-      logIn(viktor)
+      logIn(users)
     elif val == "q":
       break
+    elif val == "c":
+      createAccount(users)
     else:
       checkIput()
 
