3237ab83d0f25b12561e943a1b489ddc92eacafc
Viktor Forslund <viktor@ekastigen.net>
2024-10-14 17:58:24 +0200

    snart klart

--- TypeReccer/viktors_test.py
+++ TypeReccer/viktors_test.py
@@ -3,23 +3,41 @@ import os
 text = "Hej där mitt namn är Viktor Forslund"
 
 index = 0
+print(text)
 
 def checkLetter(index, text, input_letter, current_letter):
   reset = '\033[0m'
+  green = '\033[32m'
   red = '\033[31m'
-  if current_letter == input_letter:
-    new_letter = f'{red}{input_letter}{reset}'
-    text = text.replace(current_letter, new_letter, 1)
+  if input_letter == "space":
+    input_letter = " "
+  elif input_letter == "backspace":
+    text, new_letter = changeColor(reset, reset,input_letter, text, index)
+    index -= len(new_letter)
+
+  elif current_letter == input_letter:
+    text, new_letter = changeColor(green, reset,input_letter, text, index)
     index += len(new_letter)
+  elif current_letter != input_letter:
+    text, new_letter = changeColor(red, reset,input_letter, text, index)
+    index += len(new_letter)
+
   return index, text
+
+def changeColor(color,base_color,input_letter, text, index):
+  new_letter = f'{color}{input_letter}{base_color}'
+  text = text[:index] + new_letter + text[index + 1:]
+  return text, new_letter
 
 
 
 while True:
-  print(text)
+  
   current_letter = text[index]
   input_letter = keyboard.read_event()
   if(input_letter.event_type == keyboard.KEY_DOWN):
     input_letter = input_letter.name
-    print(index)
     index, text = checkLetter(index, text, input_letter, current_letter)
+    if input_letter != "skift":
+      os.system('cls')
+      print(text)
