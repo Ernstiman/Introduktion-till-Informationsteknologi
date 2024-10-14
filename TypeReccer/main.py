484645d753f4140e43460cffc84252118998f7c1
Viktor <viktor@ekastigen.net>
2024-10-14 20:20:11 +0200

    prototyp klar

--- TypeReccer/main.py
+++ TypeReccer/main.py
@@ -30,34 +30,62 @@ def calculateWPM():
   wpm = (index / (elapsed_time / 6)) // 5
   print(f"WPM: {wpm}")
 
-def checkLetter(index, text, input_letter, current_letter):
+def checkLetter(index, text, input_letter, current_letter = ""):
   reset = '\033[0m'
+  green = '\033[32m'
   red = '\033[31m'
-  
   if input_letter == "space":
     input_letter = " "
-  if current_letter == input_letter:
-    new_letter = f'{red}{input_letter}{reset}'
-    text = text[:index] + new_letter + text[index + 1:]
+  if input_letter == "backspace":
+    if text[index - 5] == " " and text.count(red) == 0:
+      pass
+    else: 
+      text = deleteColor(text[index - 5], text, index)
+      index -= 10
+      
+
+  elif current_letter == input_letter and text.count(red) == 0 and current_letter != "":
+    text, new_letter = changeColor(green, reset,input_letter, text, index)
     index += len(new_letter)
-  return index, text
+  elif (current_letter != input_letter or text.count(red) > 0) and current_letter != "":
+    text, new_letter = changeColor(red, reset,current_letter, text, index)
+    index += len(new_letter)
+  return index, text, red
+
+def changeColor(color,base_color,input_letter, text, index):
+  new_letter = f'{color}{input_letter}{base_color}'
+  text = text[:index] + new_letter + text[index + 1:]
+  return text, new_letter
+
+def deleteColor(input_letter, text, index):
+  text = text[:(index - 10)] + input_letter + text[index:]
+  return text
+
+def checkIndex(text, index):
+  try: 
+    letter = text[index]
+  except:
+    letter = ""
+  return letter
 
 text = generateText()
 print(text)
 counter = 0
 while True:
-  current_letter = text[index]
-  input_letter = keyboard.read_event()
-  if(input_letter.event_type == keyboard.KEY_DOWN):
-    if counter == 0:
-        start_time = time.time()
-    input_letter = input_letter.name
-    index, text = checkLetter(index, text, input_letter, current_letter)
-    os.system('cls')
-    counter += 1
-    print(text)
-  if index >= len(text):
-    break
+    current_letter = checkIndex(text, index)
+    input_letter = keyboard.read_event()
+    if(input_letter.event_type == keyboard.KEY_DOWN):
+      input_letter = input_letter.name
+      if input_letter != "skift":
+        if counter == 0:
+            start_time = time.time()
+        
+        index, text, red = checkLetter(index, text, input_letter, current_letter)
+        os.system('cls')
+        counter += 1
+        print(text)
+    if index >= len(text) and text.count(red) == 0:
+      break
 
 elapsed_time = (time.time() - start_time) 
 calculateWPM()
